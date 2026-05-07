45c17faef1427abcb94997de91ec6e5a375016f6
BMarco77 <beier.marco@web.de>
2026-05-08 00:17:49 +0200

    Update rathmerData.js

--- src/rathmer/rathmerData.js
+++ src/rathmer/rathmerData.js
@@ -14,5 +14,57 @@ export const rathmerData = {
     "Stresspunkt = 8",
     "Entspannungspunkt = 4"
   ],
-  subtypes: []
+  subtypes: [
+  {
+    code: "se2",
+    traits: ["warm", "kindlich", "Kontra"],
+    modules: [
+      "Demaskierung",
+      "Blinder Fleck",
+      "Augen",
+      "Körper",
+      "Wirkung",
+      "Weiteres",
+      "Bsp.",
+      "Lookalikes",
+      "Partnerwahl",
+      "Verkauf",
+      "Führung"
+    ]
+  },
+  {
+    code: "so2",
+    traits: ["kalt", "erwachsen", "Verst."],
+    modules: [
+      "Demaskierung",
+      "Blinder Fleck",
+      "Augen",
+      "Körper",
+      "Wirkung",
+      "Weiteres",
+      "Bsp.",
+      "Lookalikes",
+      "Partnerwahl",
+      "Verkauf",
+      "Führung"
+    ]
+  },
+  {
+    code: "sx2",
+    traits: ["hitzig", "jugendlich", "Normal"],
+    modules: [
+      "Demaskierung",
+      "Blinder Fleck",
+      "Augen",
+      "Körper",
+      "Wirkung",
+      "Weiteres",
+      "Bsp.",
+      "Lookalikes",
+      "Partnerwahl",
+      "Verkauf",
+      "Führung"
+    ]
+  }
+]
 };
