812e3bfdc2c4a96dc97c44ce5ea05e8ef3d2f588
BMarco77 <beier.marco@web.de>
2026-05-08 00:02:15 +0200

    Update rathmerData.js

--- src/rathmer/rathmerData.js
+++ src/rathmer/rathmerData.js
@@ -1,1 +1,18 @@
-
+export const rathmerData = {
+  type: 2,
+  title: "2 - Liebe",
+  center: "Herz-Menschen",
+  side: "weibliche Seite",
+  color: "#9b4dcc",
+  coreModules: [
+    "Bewusstseinsstufen",
+    "Tritypen",
+    "Leidenschaft = Stolz",
+    "Umgang",
+    "Flügel = 1",
+    "Flügel = 3",
+    "Stresspunkt = 8",
+    "Entspannungspunkt = 4"
+  ],
+  subtypes: []
+};
