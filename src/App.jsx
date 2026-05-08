f6d4e64ceef88af7974625964575a4c5245c8e0f
BMarco77 <beier.marco@web.de>
2026-05-08 11:02:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-import RathmerPrototype from "./rathmer/RathmerPrototype";
+import { useState } from "react";
+import HomePage from "./pages/HomePage";
+import TypePage from "./pages/TypePage";
 
 export default function App() {
-  return <RathmerPrototype />;
+  const [selectedType, setSelectedType] = useState(null);
+
+  if (selectedType === 2) {
+    return <TypePage onBack={() => setSelectedType(null)} />;
+  }
+
+  return <HomePage onSelectType={setSelectedType} />;
 }
