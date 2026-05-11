d388664d88a8961fd0ff02ef16e1d06e420ebcba
BMarco77 <beier.marco@web.de>
2026-05-11 19:35:44 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,24 +19,24 @@ const centers = [
     title: "Herz-Menschen",
     types: [
   { number: 2, name: "Der Helfer", color: TYPE_COLORS[2] },
-  { number: 3, name: "Erfolg", color: TYPE_COLORS[3] },
-  { number: 4, name: "Individualität", color: TYPE_COLORS[4] },
+  { number: 3, name: "Der Erfolgreiche", color: TYPE_COLORS[3] },
+  { number: 4, name: "Der Individualist", color: TYPE_COLORS[4] },
 ]
   },
   {
     title: "Kopf-Menschen",
     types: [
-      { number: 5, name: "Wissen", color: TYPE_COLORS[5] },
-      { number: 6, name: "Sicherheit", color: TYPE_COLORS[6] },
-      { number: 7, name: "Spaß", color: TYPE_COLORS[7] },
+      { number: 5, name: "Der Denker", color: TYPE_COLORS[5] },
+      { number: 6, name: "Der loyale Skeptiker", color: TYPE_COLORS[6] },
+      { number: 7, name: "Der Optimist", color: TYPE_COLORS[7] },
     ],
   },
   {
     title: "Bauch-Menschen",
     types: [
-      { number: 8, name: "Macht", color: TYPE_COLORS[8] },
-      { number: 9, name: "Harmonie", color: TYPE_COLORS[9] },
-      { number: 1, name: "Perfektion", color: TYPE_COLORS[1] },
+      { number: 8, name: "Der Anführer", color: TYPE_COLORS[8] },
+      { number: 9, name: "Der Vermittler", color: TYPE_COLORS[9] },
+      { number: 1, name: "Der Perfektionist", color: TYPE_COLORS[1] },
     ],
   },
 ];
