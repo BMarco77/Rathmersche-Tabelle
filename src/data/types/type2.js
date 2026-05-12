3a704d93b49e18a09a4fb1f32880b9a450b61d1b
BMarco77 <beier.marco@web.de>
2026-05-12 21:03:07 +0200

    Update type2.js

--- src/data/types/type2.js
+++ src/data/types/type2.js
@@ -1,90 +1,80 @@
-import { TYPE_COLORS } from "../typeColors";
-export const type2Data = {
-  type: 2,
-  title: "Liebe",
-  center: "Herz-Menschen",
-  side: "weibliche Seite",
- color: TYPE_COLORS[2],
-
-  centerInfo: {
-    
-    content:
-      `Die Herzmenschen umfassen die Typen 2, 3 und 4.
+centerInfo: {
+  content: `Die Herzmenschen umfassen die Typen 2, 3 und 4.
 
 Die Aufmerksamkeit richtet sich stark auf Beziehung, Wirkung, Identität und emotionale Resonanz.
 
 Grundproblem:
-    Liebes-Defizit
-    (Gefühl, nicht geliebt zu werden /
-    nicht anerkannt zu sein)
+- Liebes-Defizit
+- Gefühl, nicht geliebt zu werden /
+  nicht anerkannt zu sein
 
 Hauptthemen:
-    Image, Beziehungen, Empathie,
-    Leistung, Charme / Scham, Gefühle
+- Image
+- Beziehungen
+- Empathie
+- Leistung
+- Charme / Scham
+- Gefühle
 
 Identifikation:
-    „Ich bin, wie ich auf andere wirke.“`
-  },
-
-  typeInfo: {
-
-  content: `Überentwickelte Herzenergie:
-    (Herzenergie wird nach außen gerichtet)
-
-Grundmotivation:
-    Liebe
-
-Abwehrmechanismus:
-    Liebe als Tauschhandel`
+- „Ich bin, wie ich auf andere wirke.“`
 },
 
-  sideInfo: {
-        content:
-      `Die weibliche Seite des Enneagramms umfasst die Typen 1–4 und zeichnet sich durch weichere, feinere und ästhetischere Charakter- und Körperzüge aus.
+typeInfo: {
+  content: `Überentwickelte Herzenergie:
+- Herzenergie wird nach außen gerichtet
+
+Grundmotivation:
+- Liebe
+
+Abwehrmechanismus:
+- Liebe als Tauschhandel`
+},
+
+sideInfo: {
+  content: `Die weibliche Seite des Enneagramms umfasst die Typen 1–4 und zeichnet sich durch weichere, feinere und ästhetischere Charakter- und Körperzüge aus.
 
 Indizien, die hier häufiger beobachtet werden können:
 
-• Nasenform
-    zierlich, filigran, ästhetisch,
-    weich geformt, feminin
+- Nasenform
+- zierlich
+- filigran
+- ästhetisch
+- weich geformt
+- feminin
 
-• Gesichtspartie
-    weiche Haut, reine Haut,
-    lange Wimpern
+- Gesichtspartie
+- weiche Haut
+- reine Haut
+- lange Wimpern
 
-• Wirkung
-    feminin, zart, fein`
-  },
-  
-  coreModules: [
-    {
-      label: "Bewusstseinsstufen",
-      content: `Die Gesundheit des Gemüts von Typ 2 wird in folgenden Zuständen gemessen:
+- Wirkung
+- feminin
+- zart
+- fein`
+},
+
+{
+  label: "Bewusstseinsstufen",
+  content: `Die Gesundheit des Gemüts von Typ 2 wird in folgenden Zuständen gemessen:
 
 (1 = sehr unbewusst/ungesund,
 9 = sehr bewusst/gesund)
 
 1. Psychosomatisches Opfer
-
 2. Dominanter Erpresser
-
 3. Selbstbetrügerischer Manipulator
-
 4. Überheblicher Heiliger
-
 5. Besitzergreifender Intimfreund
-
 6. Großsprecherischer Freund
-
 7. Fürsorglicher
-
 8. Einfühlsamer
-
 9. Uneigennütziger Altruist`
-    },
-    {
-      label: "Tritypen",
-      content: `Der Trityp verfolgt den Sinn, dass jeder Mensch einen vorwiegenden Enneagrammtypen in jeder der drei Grundenergien aufweist.
+},
+
+{
+  label: "Tritypen",
+  content: `Der Trityp verfolgt den Sinn, dass jeder Mensch einen vorwiegenden Enneagrammtypen in jeder der drei Grundenergien aufweist.
 
 Die Kombination des Trityps setzt sich natürlich zum einen aus dem grundlegenden Enneatyp des Menschen und den jeweils vorwiegenden Typen der beiden verbleibenden Energien zusammen.
 
@@ -96,72 +86,72 @@ So stellt sich ein Dreigespann zusammen (z.B. 3-5-8), das Unterschiede in den Ei
 
 Folgende Eigenschaften werden den 9 Grundtypen zugeordnet:
 
-    Typ 1: fleißig, sorgfältig, moralisch
-    Typ 2: besorgt, mitfühlend, dienend
-    Typ 3: fokussiert, effizient, ehrgeizig
-    Typ 4: intuitiv, ursprünglich, echt, kreativ
-    Typ 5: sachkundig, wissend, weise
-    Typ 6: fragend, prüfend, unterstützend
-    Typ 7: innovativ, inspirierend, anregend
-    Typ 8: beschützend, direkt, geradlinig
-    Typ 9: akzeptierend, friedlich, friedfertig
+- Typ 1: fleißig, sorgfältig, moralisch
+- Typ 2: besorgt, mitfühlend, dienend
+- Typ 3: fokussiert, effizient, ehrgeizig
+- Typ 4: intuitiv, ursprünglich, echt, kreativ
+- Typ 5: sachkundig, wissend, weise
+- Typ 6: fragend, prüfend, unterstützend
+- Typ 7: innovativ, inspirierend, anregend
+- Typ 8: beschützend, direkt, geradlinig
+- Typ 9: akzeptierend, friedlich, friedfertig
 
 Im Fall von Typ 2 gibt es folgende Möglichkeiten für die Tritypen:
 
-    2-5-1: Mentor / Ratgeber
-    2-6-1: Unterstützer
-    2-5-9: Problemlöser
-    2-7-1: Lehrer
-    2-6-9: Guter Samariter
-    2-5-8: Stratege
-    2-7-9: Friedensstifter
-    2-6-8: Retter
-    2-7-8: Freigeist`
-    },
-    {
-      label: "Leidenschaft - Stolz",
-      content: `Bedeutung von Leidenschaft in diesem Kontext:
+- 2-5-1: Mentor / Ratgeber
+- 2-6-1: Unterstützer
+- 2-5-9: Problemlöser
+- 2-7-1: Lehrer
+- 2-6-9: Guter Samariter
+- 2-5-8: Stratege
+- 2-7-9: Friedensstifter
+- 2-6-8: Retter
+- 2-7-8: Freigeist`
+},
 
-    Pseudo-Schutz-Mechanismus,
-    der wortwörtlich „Leiden schafft“
+{
+  label: "Leidenschaft - Stolz",
+  content: `Bedeutung von Leidenschaft in diesem Kontext:
+
+- Pseudo-Schutz-Mechanismus,
+  der wortwörtlich „Leiden schafft“
 
 Bedeutung von Stolz in diesem Kontext:
 
-    Überzeugung, etwas zu leisten,
-    um Liebe zu bekommen
+- Überzeugung, etwas zu leisten,
+  um Liebe zu bekommen
 
 Gesteigerte Formen:
 
-    Hochmut
-    Scheinheiligkeit`
-    },
-    {
-      label: "Umgang",
-      content: `    Dankbarkeit zeigen
+- Hochmut
+- Scheinheiligkeit`
+},
 
-    Respekt & Rücksichtnahme
+{
+  label: "Umgang",
+  content: `- Dankbarkeit zeigen
 
-    Ehrlichkeit & Empathie,
-    um zu verhindern, dass das Gegenüber
-    sich betrogen fühlt
+- Respekt & Rücksichtnahme
 
-    emotionale Wärme suggerieren /
-    auf emotionaler Ebene begegnen
+- Ehrlichkeit & Empathie,
+  um zu verhindern, dass das Gegenüber
+  sich betrogen fühlt
 
-    „Tauschhandel“ berücksichtigen
-    und das zurückgeben, was man
-    vom Gegenüber bekommt`
-    },
-    {
-      label: "Flügel = 1",
-      content: `Bedeutung der Flügel:
+- emotionale Wärme suggerieren /
+  auf emotionaler Ebene begegnen
 
-- Die Nachbartypen eines Enneatyps
-  haben einen Einfluss auf ihn
-- Dies führt zu einer
-  Eigenschaftsadaption
-- Einer der beiden Flügel ist
-  besonders stark ausgeprägt
+- „Tauschhandel“ berücksichtigen
+  und das zurückgeben, was man
+  vom Gegenüber bekommt`
+},
+
+{
+  label: "Flügel = 1",
+  content: `Bedeutung der Flügel:
+
+- Die Nachbartypen eines Enneatyps haben einen Einfluss auf ihn
+- Dies führt zu einer Eigenschaftsadaption
+- Einer der beiden Flügel ist besonders stark ausgeprägt
 
 Flügel zu Typ 1:
 
@@ -171,7 +161,7 @@ Eigenschaften im positiven Fall:
 - Talent für die Kreation von Struktur / Ordnung
 - hohes Maß an Ehrgeiz
 - viel Fokus
- -Fähigkeit, die Rolle eines Mentors einzunehmen
+- Fähigkeit, die Rolle eines Mentors einzunehmen
 - Drang nach Optimierung / Fehlerbewusstsein
 - Überzeugungskraft
 - mentale Stärke & Disziplin
@@ -190,10 +180,11 @@ Eigenschaften im negativen Fall:
 - Konfliktsucht
 - Kritikunfähigkeit
 - zwanghafte Präsentation von Überlegenheit`
-    },
-    {
-      label: "Flügel = 3",
-      content: `Bedeutung der Flügel:
+},
+
+{
+  label: "Flügel = 3",
+  content: `Bedeutung der Flügel:
 
 - Die Nachbartypen eines Enneatyps haben einen Einfluss auf ihn
 - Dies führt zu einer Eigenschaftsadaption
@@ -227,10 +218,11 @@ Eigenschaften im negativen Fall:
 - übermäßige Identifikation mit äußeren Werten
 - Oberflächlichkeit
 - Opportunismus`
-    },
-    {
-      label: "Stresspunkt = 8",
-      content: `Definition Stresspunkt:
+},
+
+{
+  label: "Stresspunkt = 8",
+  content: `Definition Stresspunkt:
 
 Sobald ein Enneagrammtyp im außen Stress ausgesetzt ist (viel Arbeit, psychischer Stress, Überforderung, ...), kompensiert er diesen, indem er Eigenschaften von seinem sog. Stresspunkt annimmt.
 
@@ -249,10 +241,11 @@ Entwicklung im negativen Fall:
 - herrisch
 - dominant
 - aggressiv`
-    },
-    {
-      label: "Entspannungspunkt = 4",
-      content: `Definition Entspannungspunkt:
+},
+
+{
+  label: "Entspannungspunkt = 4",
+  content: `Definition Entspannungspunkt:
 
 Sobald ein Enneagrammtyp im außen Entspannung erlebt (wenig Arbeit, Urlaub, Feierabend, ...), entwickelt er sich, indem er Eigenschaften von seinem sog. Entspannungspunkt annimmt.
 
@@ -271,200 +264,4 @@ Entwicklung im negativen Fall:
 - anspruchsvoll
 - melancholisch
 - hysterisch`
-    }
-  ],
-
-  subtypes: [
-    {
-      code: "se2",
-      traits: [
-        {
-          label: "warm",
-          content: "Hier steht später die Erklärung zu warm bei se2."
-        },
-        {
-          label: "kindlich",
-          content: "Hier steht später die Erklärung zu kindlich bei se2."
-        },
-        {
-          label: "Kontra",
-          content: "Hier steht später die Erklärung zu Kontra bei se2."
-        }
-      ],
-      modules: [
-        {
-          label: "Demaskierung",
-          content: "Hier steht später der Tooltip-Text zur Demaskierung bei se2."
-        },
-        {
-          label: "Blinder Fleck",
-          content: "Hier steht später der Tooltip-Text zum blinden Fleck bei se2."
-        },
-        {
-          label: "Augen",
-          content: "Hier steht später der Tooltip-Text zu den Augen bei se2."
-        },
-        {
-          label: "Körper",
-          content: "Hier steht später der Tooltip-Text zum Körper bei se2."
-        },
-        {
-          label: "Wirkung",
-          content: "Hier steht später der Tooltip-Text zur Wirkung bei se2."
-        },
-        {
-          label: "Weiteres",
-          content: "Hier steht später weiterer Tooltip-Text bei se2."
-        },
-        {
-          label: "Bsp.",
-          content: "Hier stehen später Beispielpersonen für se2."
-        },
-        {
-          label: "Lookalikes",
-          content: "Hier stehen später Lookalikes für se2."
-        },
-        {
-          label: "Partnerwahl",
-          content: "Hier steht später der Tooltip-Text zur Partnerwahl bei se2."
-        },
-        {
-          label: "Verkauf",
-          content: "Hier steht später der Tooltip-Text zum Verkauf bei se2."
-        },
-        {
-          label: "Führung",
-          content: "Hier steht später der Tooltip-Text zur Führung bei se2."
-        }
-      ]
-    },
-
-    {
-      code: "so2",
-      traits: [
-        {
-          label: "kalt",
-          content: "Hier steht später die Erklärung zu kalt bei so2."
-        },
-        {
-          label: "erwachsen",
-          content: "Hier steht später die Erklärung zu erwachsen bei so2."
-        },
-        {
-          label: "Verst.",
-          content: "Hier steht später die Erklärung zu Verst. bei so2."
-        }
-      ],
-      modules: [
-        {
-          label: "Demaskierung",
-          content: "Hier steht später der Tooltip-Text zur Demaskierung bei so2."
-        },
-        {
-          label: "Blinder Fleck",
-          content: "Hier steht später der Tooltip-Text zum blinden Fleck bei so2."
-        },
-        {
-          label: "Augen",
-          content: "Hier steht später der Tooltip-Text zu den Augen bei so2."
-        },
-        {
-          label: "Körper",
-          content: "Hier steht später der Tooltip-Text zum Körper bei so2."
-        },
-        {
-          label: "Wirkung",
-          content: "Hier steht später der Tooltip-Text zur Wirkung bei so2."
-        },
-        {
-          label: "Weiteres",
-          content: "Hier steht später weiterer Tooltip-Text bei so2."
-        },
-        {
-          label: "Bsp.",
-          content: "Hier stehen später Beispielpersonen für so2."
-        },
-        {
-          label: "Lookalikes",
-          content: "Hier stehen später Lookalikes für so2."
-        },
-        {
-          label: "Partnerwahl",
-          content: "Hier steht später der Tooltip-Text zur Partnerwahl bei so2."
-        },
-        {
-          label: "Verkauf",
-          content: "Hier steht später der Tooltip-Text zum Verkauf bei so2."
-        },
-        {
-          label: "Führung",
-          content: "Hier steht später der Tooltip-Text zur Führung bei so2."
-        }
-      ]
-    },
-
-    {
-      code: "sx2",
-      traits: [
-        {
-          label: "hitzig",
-          content: "Hier steht später die Erklärung zu hitzig bei sx2."
-        },
-        {
-          label: "jugendlich",
-          content: "Hier steht später die Erklärung zu jugendlich bei sx2."
-        },
-        {
-          label: "Normal",
-          content: "Hier steht später die Erklärung zu Normal bei sx2."
-        }
-      ],
-      modules: [
-        {
-          label: "Demaskierung",
-          content: "Hier steht später der Tooltip-Text zur Demaskierung bei sx2."
-        },
-        {
-          label: "Blinder Fleck",
-          content: "Hier steht später der Tooltip-Text zum blinden Fleck bei sx2."
-        },
-        {
-          label: "Augen",
-          content: "Hier steht später der Tooltip-Text zu den Augen bei sx2."
-        },
-        {
-          label: "Körper",
-          content: "Hier steht später der Tooltip-Text zum Körper bei sx2."
-        },
-        {
-          label: "Wirkung",
-          content: "Hier steht später der Tooltip-Text zur Wirkung bei sx2."
-        },
-        {
-          label: "Weiteres",
-          content: "Hier steht später weiterer Tooltip-Text bei sx2."
-        },
-        {
-          label: "Bsp.",
-          content: "Hier stehen später Beispielpersonen für sx2."
-        },
-        {
-          label: "Lookalikes",
-          content: "Hier stehen später Lookalikes für sx2."
-        },
-        {
-          label: "Partnerwahl",
-          content: "Hier steht später der Tooltip-Text zur Partnerwahl bei sx2."
-        },
-        {
-          label: "Verkauf",
-          content: "Hier steht später der Tooltip-Text zum Verkauf bei sx2."
-        },
-        {
-          label: "Führung",
-          content: "Hier steht später der Tooltip-Text zur Führung bei sx2."
-        }
-      ]
-    }
-  ]
-};
+},
