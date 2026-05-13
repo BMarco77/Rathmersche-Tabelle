db1fc86e73846c651bbfbbbb50c969760ff4c4e4
BMarco77 <beier.marco@web.de>
2026-05-13 21:19:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import HomePage from "./pages/HomePage";
 import TypePage from "./pages/TypePage";
+import Type3Page from "./pages/Type3Page";
 
 export default function App() {
   const [selectedType, setSelectedType] = useState(null);
 
   if (selectedType === 2) {
     return <TypePage onBack={() => setSelectedType(null)} />;
+  }
+
+  if (selectedType === 3) {
+    return <Type3Page onBack={() => setSelectedType(null)} />;
   }
 
   return <HomePage onSelectType={setSelectedType} />;
