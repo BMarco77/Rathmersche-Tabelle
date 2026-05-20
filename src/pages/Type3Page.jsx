d414bedc50d01c8ddbcf5789f741558b9db98c5a
BMarco77 <beier.marco@web.de>
2026-05-20 14:12:29 +0200

    Update Type3Page.jsx

--- src/pages/Type3Page.jsx
+++ src/pages/Type3Page.jsx
@@ -359,7 +359,7 @@ export default function TypePage({ onBack }) {
   style={{
     background: selectedSubtypeItem
       ? undefined
-      : TYPE_GRADIENTS[type3Data.type],
+      : TYPE_INFO_GRADIENTS[type3Data.type],
   }}
 >
           {selectedSubtypeItem ? (
