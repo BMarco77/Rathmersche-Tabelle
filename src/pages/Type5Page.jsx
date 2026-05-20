ed22215cfb4b09c96e7d7c66ae3b38c6fb69ca12
BMarco77 <beier.marco@web.de>
2026-05-20 14:13:03 +0200

    Update Type5Page.jsx

--- src/pages/Type5Page.jsx
+++ src/pages/Type5Page.jsx
@@ -359,7 +359,7 @@ export default function TypePage({ onBack }) {
   style={{
     background: selectedSubtypeItem
       ? undefined
-      : TYPE_GRADIENTS[type5Data.type],
+      : TYPE_INFO_GRADIENTS[type5Data.type],
   }}
 >
           {selectedSubtypeItem ? (
