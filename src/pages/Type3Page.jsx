09b0184c9b251edbced474f2eb1adcb682f91bda
BMarco77 <beier.marco@web.de>
2026-05-20 23:03:48 +0200

    Update Type3Page.jsx

--- src/pages/Type3Page.jsx
+++ src/pages/Type3Page.jsx
@@ -4,7 +4,7 @@ import { type3Data } from "../data/types/type3";
 import { TYPE_GRADIENTS, TYPE_INFO_GRADIENTS } from "../data/typeColors";
 import "../rathmer/rathmer.css";
 
-export default function TypePage({ onBack }) {
+export default function TypePage({ onBack, onSelectType }) {
   const [openTopItems, setOpenTopItems] = useState([]);
 
   useEffect(() => {
