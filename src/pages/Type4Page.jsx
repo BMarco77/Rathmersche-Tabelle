018cf7a9b1c414d4a4f9d4ab4e43317e65f94c7d
BMarco77 <beier.marco@web.de>
2026-05-20 23:04:11 +0200

    Update Type4Page.jsx

--- src/pages/Type4Page.jsx
+++ src/pages/Type4Page.jsx
@@ -4,7 +4,7 @@ import { type4Data } from "../data/types/type4";
 import { TYPE_GRADIENTS, TYPE_INFO_GRADIENTS } from "../data/typeColors";
 import "../rathmer/rathmer.css";
 
-export default function TypePage({ onBack }) {
+export default function TypePage({ onBack, onSelectType }) {
   const [openTopItems, setOpenTopItems] = useState([]);
 
   useEffect(() => {
