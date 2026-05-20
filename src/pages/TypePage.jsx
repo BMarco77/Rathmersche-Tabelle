9ab04b046d0479d5438abaddbdf79ab0023e78f6
BMarco77 <beier.marco@web.de>
2026-05-20 22:06:53 +0200

    Update TypePage.jsx

--- src/pages/TypePage.jsx
+++ src/pages/TypePage.jsx
@@ -15,7 +15,7 @@ export default function TypePage({ onBack }) {
   const [selectedSubtypeItem, setSelectedSubtypeItem] = useState(null);
   const [openSubtype, setOpenSubtype] = useState(null);
 
-  const currentType = type4Data.type;
+  const currentType = type2Data.type;
 
   const prevType = currentType === 1 ? 9 : currentType - 1;
   const nextType = currentType === 9 ? 1 : currentType + 1;
