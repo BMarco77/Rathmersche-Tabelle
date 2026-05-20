01de7f52df356c71dc0f2d7a657bfd1fdb6db14d
BMarco77 <beier.marco@web.de>
2026-05-20 13:43:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Type5Page from "./pages/Type5Page";
 import Type6Page from "./pages/Type6Page";
 import Type7Page from "./pages/Type7Page";
 import Type8Page from "./pages/Type8Page";
+import Type9Page from "./pages/Type9Page";
 
 
 export default function App() {
@@ -16,34 +17,29 @@ export default function App() {
   if (selectedType === 2) {
     return <TypePage onBack={() => setSelectedType(null)} />;
   }
-
   if (selectedType === 3) {
     return <Type3Page onBack={() => setSelectedType(null)} />;
   }
-
-   if (selectedType === 1) {
+  if (selectedType === 1) {
     return <Type1Page onBack={() => setSelectedType(null)} />;
   }
-
-   if (selectedType === 4) {
+  if (selectedType === 4) {
     return <Type4Page onBack={() => setSelectedType(null)} />;
   }
-
-   if (selectedType === 5) {
+  if (selectedType === 5) {
     return <Type5Page onBack={() => setSelectedType(null)} />;
   }
-
   if (selectedType === 6) {
     return <Type6Page onBack={() => setSelectedType(null)} />;
   }
-
-   if (selectedType === 7) {
+  if (selectedType === 7) {
     return <Type7Page onBack={() => setSelectedType(null)} />;
   }
-
   if (selectedType === 8) {
     return <Type8Page onBack={() => setSelectedType(null)} />;
   }
-
+  if (selectedType === 9) {
+    return <Type9Page onBack={() => setSelectedType(null)} />;
+  }
   return <HomePage onSelectType={setSelectedType} />;
 }
