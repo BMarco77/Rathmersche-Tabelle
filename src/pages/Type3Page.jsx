b6061d46d7d4d83ef06f6b95bfcf58de4317402e
BMarco77 <beier.marco@web.de>
2026-05-21 12:40:15 +0200

    Update Type3Page.jsx

--- src/pages/Type3Page.jsx
+++ src/pages/Type3Page.jsx
@@ -223,7 +223,9 @@ export default function TypePage({ onBack, onSelectType }) {
                   active ? "is-active" : ""
                 }`}
                 style={{
-  borderColor: TYPE_SHADOWS[type3Data.type],
+  borderColor: active
+    ? TYPE_SHADOWS[type3Data.type]
+    : `${TYPE_SHADOWS[type3Data.type]}55`,
 }}
                 onClick={() =>
                   toggleItem(
