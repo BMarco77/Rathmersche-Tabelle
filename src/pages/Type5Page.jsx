d3bd3838e1677a647387675e939f9cb05bd41888
BMarco77 <beier.marco@web.de>
2026-05-21 17:45:13 +0200

    Update Type5Page.jsx

--- src/pages/Type5Page.jsx
+++ src/pages/Type5Page.jsx
@@ -225,7 +225,8 @@ const nextType = currentType === 9 ? 1 : currentType + 1;
                 }`}
                 style={{
   borderColor: active
-    ? TYPE_SHADOWS[type5Data.type],
+    ? TYPE_GLOW_COLORS[type5Data.type]
+    : "transparent",
 }}
                 onClick={() =>
                   toggleItem(
