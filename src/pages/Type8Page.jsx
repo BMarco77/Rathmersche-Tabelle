0c3427fdbd68309d172517f8253d0edf3e6f2ecd
BMarco77 <beier.marco@web.de>
2026-05-21 09:57:08 +0200

    Update Type8Page.jsx

--- src/pages/Type8Page.jsx
+++ src/pages/Type8Page.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import wappen from "../assets/wappen-512.png";
 import { type8Data } from "../data/types/type8";
-import { TYPE_GRADIENTS, TYPE_INFO_GRADIENTS } from "../data/typeColors";
 import "../rathmer/rathmer.css";
 import {
   TYPE_GRADIENTS,
