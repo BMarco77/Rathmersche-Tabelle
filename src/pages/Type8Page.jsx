87af2117edaf216c9df2409420ddcb21a407ef92
BMarco77 <beier.marco@web.de>
2026-05-21 21:58:27 +0200

    Update Type8Page.jsx

--- src/pages/Type8Page.jsx
+++ src/pages/Type8Page.jsx
@@ -1,7 +1,7 @@
 import TypePage from "../rathmer/TypePage";
 import { type8Data } from "../data/types/type8";
 
-export default function Type5Page({ onBack, onSelectType }) {
+export default function Type8Page({ onBack, onSelectType }) {
   return (
     <TypePage
       typeData={type8Data}
