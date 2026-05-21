e781d9cf1c31fb2e2289c9ed29d510fb3e1b804d
BMarco77 <beier.marco@web.de>
2026-05-21 21:59:12 +0200

    Update Type3Page.jsx

--- src/pages/Type3Page.jsx
+++ src/pages/Type3Page.jsx
@@ -1,7 +1,7 @@
 import TypePage from "../rathmer/TypePage";
 import { type3Data } from "../data/types/type3";
 
-export default function Type1Page({ onBack, onSelectType }) {
+export default function Type3Page({ onBack, onSelectType }) {
   return (
     <TypePage
       typeData={type3Data}
