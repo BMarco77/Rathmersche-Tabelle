0afd46011cf58718d7da6839fec8f33ea1e0dfb5
BMarco77 <beier.marco@web.de>
2026-05-21 17:52:06 +0200

    Update Type1Page.jsx

--- src/pages/Type1Page.jsx
+++ src/pages/Type1Page.jsx
@@ -5,7 +5,8 @@ import "../rathmer/rathmer.css";
 import {
   TYPE_GRADIENTS,
   TYPE_INFO_GRADIENTS,
-  TYPE_SHADOWS,} from "../data/typeColors";
+  TYPE_GLOW_COLORS
+} from "../data/typeColors";
 
 export default function TypePage({ onBack, onSelectType }) {
   const [openTopItems, setOpenTopItems] = useState([]);
