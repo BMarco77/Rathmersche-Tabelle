db017cef8267e9e5c429007455d6b70faed751e9
BMarco77 <beier.marco@web.de>
2026-05-21 09:56:19 +0200

    Update Type3Page.jsx

--- src/pages/Type3Page.jsx
+++ src/pages/Type3Page.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import wappen from "../assets/wappen-512.png";
 import { type3Data } from "../data/types/type3";
-import { TYPE_GRADIENTS, TYPE_INFO_GRADIENTS } from "../data/typeColors";
 import "../rathmer/rathmer.css";
 import {
   TYPE_GRADIENTS,
