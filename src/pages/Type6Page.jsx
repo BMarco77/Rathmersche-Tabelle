01b7c6d3213b9345ccedf80e1109ff4c2c86feb8
BMarco77 <beier.marco@web.de>
2026-05-21 09:46:36 +0200

    Update Type6Page.jsx

--- src/pages/Type6Page.jsx
+++ src/pages/Type6Page.jsx
@@ -3,6 +3,11 @@ import wappen from "../assets/wappen-512.png";
 import { type6Data } from "../data/types/type6";
 import { TYPE_GRADIENTS, TYPE_INFO_GRADIENTS } from "../data/typeColors";
 import "../rathmer/rathmer.css";
+import {
+  TYPE_GRADIENTS,
+  TYPE_INFO_GRADIENTS,
+  TYPE_SHADOWS,
+} from "../data/typeColors";
 
 export default function TypePage({ onBack, onSelectType }) {
   const [openTopItems, setOpenTopItems] = useState([]);
