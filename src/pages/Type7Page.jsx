6f6eb733ce8321ada4a70237cdd2955c8e6a46df
BMarco77 <beier.marco@web.de>
2026-05-21 21:58:37 +0200

    Update Type7Page.jsx

--- src/pages/Type7Page.jsx
+++ src/pages/Type7Page.jsx
@@ -1,7 +1,7 @@
 import TypePage from "../rathmer/TypePage";
 import { type7Data } from "../data/types/type7";
 
-export default function Type5Page({ onBack, onSelectType }) {
+export default function Type7Page({ onBack, onSelectType }) {
   return (
     <TypePage
       typeData={type7Data}
