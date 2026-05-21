5581edd44bddadc31f4bf586a83c78485687f247
BMarco77 <beier.marco@web.de>
2026-05-21 18:09:28 +0200

    Update Type4Page.jsx

--- src/pages/Type4Page.jsx
+++ src/pages/Type4Page.jsx
@@ -253,11 +253,10 @@ export default function TypePage({ onBack, onSelectType }) {
             <button
               id={`subtype-${subtype.code}`}
               className={`rathmer-subtype-header ${
-                openSubtype === subtype.code ? "is-open" : ""
-              }`}
-            style={{
+  openSubtype === subtype.code ? "is-open" : ""
+}`}
+style={{
   background: TYPE_GRADIENTS[type4Data.type],
-  boxShadow: `0 16px 30px ${TYPE_SHADOWS[type4Data.type]}`,
 }}
               onClick={() => {
                 if (openSubtype === subtype.code) {
