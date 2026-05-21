3674667a1e7b60a2c9098d368076a3d62bdde089
BMarco77 <beier.marco@web.de>
2026-05-21 21:59:01 +0200

    Update Type4Page.jsx

--- src/pages/Type4Page.jsx
+++ src/pages/Type4Page.jsx
@@ -1,7 +1,7 @@
 import TypePage from "../rathmer/TypePage";
 import { type4Data } from "../data/types/type4";
 
-export default function Type1Page({ onBack, onSelectType }) {
+export default function Type4Page({ onBack, onSelectType }) {
   return (
     <TypePage
       typeData={type4Data}
