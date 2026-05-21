6879742c55a474d51c4fd72372a1c41d4331ff72
BMarco77 <beier.marco@web.de>
2026-05-21 11:33:30 +0200

    Update Type3Page.jsx

--- src/pages/Type3Page.jsx
+++ src/pages/Type3Page.jsx
@@ -222,6 +222,9 @@ export default function TypePage({ onBack, onSelectType }) {
                 className={`rathmer-theory-button ${
                   active ? "is-active" : ""
                 }`}
+                style={{
+  borderColor: TYPE_SHADOWS[type3Data.type],
+}}
                 onClick={() =>
                   toggleItem(
                     openTheoryRow2Items,
