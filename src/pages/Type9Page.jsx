0197c729dbfa199bd455cad56ad0718e6aa34238
BMarco77 <beier.marco@web.de>
2026-05-21 09:49:14 +0200

    Update Type9Page.jsx

--- src/pages/Type9Page.jsx
+++ src/pages/Type9Page.jsx
@@ -254,6 +254,7 @@ export default function TypePage({ onBack, onSelectType }) {
               }`}
               style={{
   background: TYPE_GRADIENTS[type9Data.type],
+  boxShadow: `0 16px 30px ${TYPE_SHADOWS[type9Data.type]}`,
 }}
               onClick={() => {
                 if (openSubtype === subtype.code) {
