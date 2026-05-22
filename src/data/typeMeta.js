5b08289e3d36a6a6fdf7cfe2ef8670d30dc75a4b
BMarco77 <beier.marco@web.de>
2026-05-22 09:22:04 +0200

    Create typeMeta.js

---
+++ src/data/typeMeta.js
@@ -0,0 +1,11 @@
+export const TYPE_META = {
+  1: { archetype: "Der Perfektionist", title: "Ordnung" },
+  2: { archetype: "Der Helfer", title: "Liebe" },
+  3: { archetype: "Der Erfolgreiche", title: "Erfolg" },
+  4: { archetype: "Der Individualist", title: "Identität" },
+  5: { archetype: "Der Denker", title: "Wissen" },
+  6: { archetype: "Der loyale Skeptiker", title: "Sicherheit" },
+  7: { archetype: "Der Optimist", title: "Spaß" },
+  8: { archetype: "Der Anführer", title: "Macht" },
+  9: { archetype: "Der Vermittler", title: "Frieden" },
+};
