d4fc0c26bc7aabdde0722882fa75b4621f9a142a
BMarco77 <beier.marco@web.de>
2026-06-05 07:39:42 +0200

    Update type7.js

--- src/data/types/type7.js
+++ src/data/types/type7.js
@@ -3,13 +3,13 @@ import { TYPE_COLORS, TYPE_GRADIENTS } from "../typeColors";
 export const type7Data = {
   type: 7,
   title: "Spaß",
-  center: "Kopf-Mensch",
+  center: "Kopfmensch",
   side: "Männliche Seite",
   color: TYPE_COLORS[7],
 
   centerInfo: {
     content: `Grundproblem: 
-Vertrauens-Defizit (Gefühl, nicht vertrauen zu können / unsicher & ängstlich zu sein)
+Vertrauensdefizit (Gefühl, nicht vertrauen zu können / unsicher & ängstlich zu sein)
 
 Hauptthemen:
 - Rationalismus 
