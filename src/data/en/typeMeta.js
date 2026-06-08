20d885b740f2f945eda32500d8faf2c35f489f6b
BMarco77 <beier.marco@web.de>
2026-06-08 12:42:41 +0200

    Create typeMeta.js

---
+++ src/data/en/typeMeta.js
@@ -0,0 +1,11 @@
+export const TYPE_META = {
+  1: { archetype: "The Perfectionist", title: "Order" },
+  2: { archetype: "The Helper", title: "Love" },
+  3: { archetype: "The Achiever", title: "Success" },
+  4: { archetype: "The Individualist", title: "Identity" },
+  5: { archetype: "The Thinker", title: "Knowledge" },
+  6: { archetype: "The Loyal Skeptic", title: "Security" },
+  7: { archetype: "The Optimist", title: "Fun" },
+  8: { archetype: "The Leader", title: "Power" },
+  9: { archetype: "The Mediator", title: "Peace" },
+};
