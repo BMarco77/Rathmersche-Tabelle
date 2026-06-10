7565ce0cb57ab4b13a21314464b20ee493c442c0
BMarco77 <beier.marco@web.de>
2026-06-10 13:19:49 +0200

    Update type7.js

--- src/data/en/type7.js
+++ src/data/en/type7.js
@@ -488,7 +488,7 @@ Confusion in female form:
       label: "Choice of Partner",
       content: `DISCLAIMER: Enneatypes not listed here are not necessarily bad partners! We only see tendencies here.
 
-Statistically speaking, the so7 is most likely to choose the following partners:
+Statistically speaking, the se7 is most likely to choose the following partners:
 
 - se/so/sx9
 - se/so6
